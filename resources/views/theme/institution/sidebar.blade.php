72b864993158dd193c11eec38a8b7523353ccd28
devlopersusmita <susmita.elvirainfotech@gmail.com>
2023-09-01 15:39:58 +0530

    sidebar page changes

--- resources/views/theme/institution/sidebar.blade.php
+++ resources/views/theme/institution/sidebar.blade.php
@@ -58,7 +58,7 @@
 									<span>Teachers Page</span>
 									<img class="metismenu-state-icon" src="images/setting-icon.svg">
 								</a>
-								<div  class="{{ Request::routeIs('assigncourserequest') ? 'hide-menu' : '' }}">
+								<div class="hide-menu">
 
 									<a href="{{ route('institutionmyteacher', ['institution_id' => $_GET['institution_id']]) }}" class="{{ Request::routeIs('institutionmyteacher') ? 'submenu-active-link' : '' }}">
 										<img class="metismenu-icon" src="images/icon-arrow-right2.svg">
