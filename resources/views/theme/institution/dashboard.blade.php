e73df8b5f48b1f70977f8c885a5b22dce512a788
devlopersusmita <susmita.elvirainfotech@gmail.com>
2023-09-12 11:21:27 +0530

    institution folder dashboard file added

---
+++ resources/views/theme/institution/dashboard.blade.php
@@ -0,0 +1,26 @@
+
+@extends('theme.institution.default')
+
+@section('content')
+
+		<div class="app-main">
+        @include('theme.institution.sidebar')
+			<div class="app-main__outer">
+				<div class="app-main__inner">
+
+
+				</div>
+
+				<div class="app-wrapper-footer">
+					<div class="app-footer">
+						<div class="app-footer__inner">
+							<div class="app-footer-left">
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</div>
+	</div>
+        @endsection
