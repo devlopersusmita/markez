70f37a409c5dad000ba2f8a08683e4189f7ae68f
devlopersusmita <susmita.elvirainfotech@gmail.com>
2023-09-15 16:03:47 +0530

    swebphp changes

--- routes/web.php
+++ routes/web.php
@@ -145,6 +145,7 @@ Route::post('studentteacherstudentsend/{id}/{type}',  [HomeController::class,'st
 Route::post('studentinstitutionstudentsend/{id}/{type}',  [HomeController::class,'studentinstitutionstudentsend'])->name('studentinstitutionstudentsend');
 //start INstitution register//
 Route::get('/register/step1', [RegisterController::class, 'showStep1'])->name('register.step1');
+Route::post('/Step1submit', [RegisterController::class, 'Step1submit'])->name('Step1submit');
 Route::post('/register/step2', [RegisterController::class,'showStep2'])->name('register.step2');
 Route::post('/register/step3',  [RegisterController::class,'showStep3'])->name('register.step3');
 Route::post('/register/submit',  [RegisterController::class,'submit'])->name('register.submit');
