03c1b0c4f89d1c546e6c51521152f7a87a4b862a
devlopersusmita <susmita.elvirainfotech@gmail.com>
2023-09-18 11:28:17 +0530

    course content blade changes

--- resources/views/theme/institution/coursecontent.blade.php
+++ resources/views/theme/institution/coursecontent.blade.php
@@ -1,112 +1,82 @@
 @extends('theme.institution.default')
-
 @section('content')
+<div class="app-main">
+@include('theme.institution.sidebar')
+
+<div class="app-main__outer">
+   <div class="app-main__inner">
+
+    <div class="category-top-row">
+      <h2>Course Contents</h2>
+
+    </div>
+
+    <div class="table-responsive category-table">
+      <table class="table course-table">
+        <thead>
+        <th>Title</th>
+                <th>Slug</th>
+                <th>Type</th>
+                <th>Start Date</th>
+                <th>End Date</th>
 
 
- <div class="view-wrapper">
+                <th>Actions</th>
+        </thead>
+        <tbody>
 
-        <!-- Container -->
-        <div class="container is-custom">
-
-            <!- <div class="view-wrapper">
-
-                <!-- Container -->
-         <div class="container is-custom">
-
-             <!-- Profile page main wrapper -->
-             <div id="profile-about" class="view-wrap is-headless">
-                 <div class="columns is-multiline no-margin">
-                     <!-- Left side column -->
-                     <div class="column is-paddingless">
-                         <!-- Timeline Header -->
-
-                     </div>
-
-                 </div>
-
-                 <div class="column">
-
-                     <!-- About sections -->
-                     <div class="profile-about side-menu">
-                         @include('theme.institution.sidebar')
-                         <div class="right-content">
-                            <div class="groups-grid padding_0">
-
-                                <div class="grid-header">
-                                    <div class="header-inner">
-                                        <h2>Course Contents</h2>
-                                        <div class="header-actions">
-                                             <div class="buttons">
-
-                                            </div>
-                                        </div>
-                                    </div>
-                                     <div class="header-inner padding_top_10">
-                                        <h2>&nbsp;</h2>
-
-                                        <div class="header-actions">
-
-                                        <form id="searchform" name="searchform" >
-                                    <div class="field is-grouped">
-                                        <div class="control" >
-                                            <input type="text" name="title" class="input" placeholder="Title" />
-                                        </div>
-                                        <div class="control" >
-                                            <a class="button is-solid primary-button raised"  href='{{Route("institutioncoursecontent",["id"=>$course_id])}}' id='search_btn'>Search</a>
-                                        </div>
-                                    </div>
-                                   </form>
-
-                                        </div>
-
-                                    </div>
-                                    <div class="header-inner padding_top_10">
+        @foreach($coursecontents as $course)
+            <tr >
+                <td>{{$course['title']}}  </td>
+                <td>{{$course['slug']}}  </td>
+                <td>{{$course['type']}}  </td>
+                <td>{{$course['start_date']}}  </td>
+                <td>{{$course['end_date']}}  </td>
 
 
-                                        <div class="header-actions">
+                <td>
+                    <div class="ui-elements">
+                       <div class="buttons">
+                    <table>
+                             <tr>
+                                @if($course['type']=='zoom')
+                                <td style="padding: 0px 5px;">
+                                     <a href="{{Route('institutioncourseonline_classes',['id'=>$course['course_id'],'content_id'=>$course['id']])}}"  class="button is-solid green-button raised"   style="cursor: pointer;"  >Online Class</a>
+                                 </td>
+                                 @endif
+                                 @if($course['type']=='quiz')
+
+                                <td style="padding: 0px 5px;">
 
 
-                                                <div class="field is-grouped">
-                                                    <div class="control" >
-                                                        <a href="{{route('institutioncourse')}}"><h1>{{$course_details->title}}</h1></a>
-                                                    </div>
-
-                                                </div>
-
-
-                                        </div>
-
-                                    </div>
-
-                                </div>
-                            </div>
-                             <div id="overview-content" class="content-section is-active">
-
-                                <div id="pagination_data">
-                                    @include("theme.institution.coursecontent-pagination",['coursecontents'=>$coursecontents,'course_id'=>$course_id,'type'=>$type,'course_details'=>$course_details])
-                                </div>
-                            </div>
+                                    <a href="{{Route('institutioncoursecontentquize',['id'=>$course['course_id'],'content_id'=>$course['id']])}}" class="button is-solid blue-button raised"   style="cursor: pointer;" >Quiz</a>
 
 
 
+                                </td>
+
+@endif
+                                    <td style="padding: 0px 5px;">
+                                        <span   class="button is-solid blue-button raised coursecontentview_modal"  data-toggle="modal" data-target="#modals-view" style="cursor: pointer;" data-id="<?php echo $course['id']?>" >View</span>
+                                    </td>
+                             </tr>
+                     </table>
+                       </div>
+                   </div>
 
 
-                         </div>
-
-                     </div>
-                 </div>
-             </div>
-
-            </div>
-                 </div>
-
-
-
-
-
+               </td>
 
 
 
+            </tr>
+
+           @endforeach
+        </tbody>
+      </table>
+    </div>
+
+    @endsection
  <!-- Modal to view  Course starts-->
  <div id="modals-view"  class="modal change-cover-modal is-medium has-light-bg">
     <div class="modal-background" ></div>
@@ -228,7 +198,3 @@
 
 
 
-    </div>
-
-
-@endsection
