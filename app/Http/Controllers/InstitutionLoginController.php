dcf7a0538b8cb14d2dbaeb2bba2e8700cdb10fb8
devlopersusmita <susmita.elvirainfotech@gmail.com>
2023-09-18 13:28:10 +0530

    institution login controller page changes

--- app/Http/Controllers/InstitutionLoginController.php
+++ app/Http/Controllers/InstitutionLoginController.php
@@ -1540,7 +1540,7 @@ public function student(Request $request)
         $user_id = $request->user_id;
             //dd($user_id);
            $check_course_accessibility_by_institution = $this->check_course_accessibility_by_institution($course_id,$user_id);
-           dd($check_course_accessibility_by_institution);
+
            if($check_course_accessibility_by_institution){
             $course_details = Course::where('id',$course_id)->first();
             //dd($course_details);
---
+++ resources/views/theme/institution/no_access_course.php
@@ -0,0 +1,13 @@
+@extends('theme.institution.default')
+@section('content')
+<div class="app-main">
+@include('theme.institution.sidebar')
+
+<div class="app-main__outer">
+   <div class="app-main__inner">
+
+    <div class="category-top-row">
+      <h2>No Access Course</h2>
+
+    </div>
+
