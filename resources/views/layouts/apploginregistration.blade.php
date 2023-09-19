7c5a81386dfac3803146280fb431e59c1f3d6236
devlopersusmita <susmita.elvirainfotech@gmail.com>
2023-09-19 15:01:36 +0530

    changes

--- resources/views/layouts/apploginregistration.blade.php
+++ resources/views/layouts/apploginregistration.blade.php
@@ -260,10 +260,10 @@ if (typeof (FileReader) != "undefined") {
             passwordError = false;
             return false;
         }
-        if (passwordValue.length < 3) {
+        if (passwordValue.length < 3 ) {
             $("#passcheck").show();
             $("#passcheck").html(
-                "**length of your password must be 3"
+                "**length of your password must be 3 "
             );
             $("#passcheck").css("color", "red");
             passwordError = false;
