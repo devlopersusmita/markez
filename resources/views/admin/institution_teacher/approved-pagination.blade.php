bb5fdcc2db38449e338606f85be15f65444d1bed
devlopersusmita <susmita.elvirainfotech@gmail.com>
2023-09-20 13:24:19 +0530

    changes

--- resources/views/admin/institution_teacher/approved-pagination.blade.php
+++ resources/views/admin/institution_teacher/approved-pagination.blade.php
@@ -1,13 +1,8 @@
-
-
-@if($data->count()==0)
-    <div class="review_filter">
-        <h3>No Data Found</h3>
-      </div>
+@if(!empty($data))
 @include('frontend.notification')
 
 
-@else
+
 <div class="card">
 <div class="card-body table-responsive">
     <table id="example1_123" class="table table-bordered table-striped">
