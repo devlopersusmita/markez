46c7716f432cc5f186c3dadca98d0a61f1616fc3
devlopersusmita <susmita.elvirainfotech@gmail.com>
2023-09-21 17:13:21 +0530

    sadsadsa

--- resources/views/admin/privacypolicy/privacypolicy-pagination.blade.php
+++ resources/views/admin/privacypolicy/privacypolicy-pagination.blade.php
@@ -11,14 +11,15 @@
     <div class="row">
         <div class="col-md-6">
             <label>Privacy Policy</label>
-            <p id="show_name">{!!@$privacypolicy->privacy_policy!!}</p>
+            <p id="show_name">{!!@$privacypolicys->privacy_policy!!}</p>
         </div>
 
         <div class="col-md-12 mt-3 text-right"  >
-        <?php if(@$privacypolicy->id) { ?>
-        <span class="btn btn-warning privacypolicyedit_modal" data-toggle="modal" data-target="#modals-edit" style="cursor: pointer;" data-id="<?php echo @$privacypolicy->id?>" >Update</span>
+        (<?php echo @$privacypolicys[0]->id; ?>)
+        <?php if(@$privacypolicys->id) { ?>
+        <span class="btn btn-warning privacypolicyedit_modal" data-toggle="modal" data-target="#modals-edit" style="cursor: pointer;" data-id="<?php echo @$privacypolicys->id?>" >Update</span>
         <?php  } else { ?>
-            <span class="btn btn-warning privacypolicyedit_modal" data-toggle="modal" data-target="#modals-edit" style="cursor: pointer;" data-id="0" >Add (<?php echo @$privacypolicy[0]->id; ?>)</span>
+            <span class="btn btn-warning privacypolicyedit_modal" data-toggle="modal" data-target="#modals-edit" style="cursor: pointer;" data-id="0" >Add</span>
             <?php } ?>
               </div>
     </div>
