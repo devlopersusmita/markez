be885af4d5044d48c2efde770b92dc940dbcc933
devlopersusmita <susmita.elvirainfotech@gmail.com>
2023-09-22 16:04:03 +0530

    institution login controller changes

--- app/Http/Controllers/InstitutionLoginController.php
+++ app/Http/Controllers/InstitutionLoginController.php
@@ -3579,9 +3579,9 @@ public function institutiondashboard(Request $request)
             }
 
           $current_date = date('Y-m-d H:i:s');
-                $course_count = Course::where(['user_id'=>$user_id])->orderBy('id','asc')->get()->count();
+                $course_count = Course::where(['institution_id'=>$user_id])->orderBy('id','asc')->get()->count();
                 //dd($courses);
-                $total_payment = Payment::where(['user_id'=>$user_id])->orderBy('id','asc')->get()->sum('amount');
+                $total_payment = Payment::where(['institution_id'=>$user_id])->orderBy('id','asc')->get()->sum('amount');
                 //dd($total_payment);
                 $total_student = InstitutionStudent::where(['institution_id'=>$user_id])->orderBy('id','asc')->get()->count();
                 //dd($total_student);
