19c1df7154a556706c2d34fc94c3f9a407e374c5
devlopersusmita <susmita.elvirainfotech@gmail.com>
2023-09-27 11:58:51 +0530

    institution login controller chnages

--- app/Http/Controllers/InstitutionLoginController.php
+++ app/Http/Controllers/InstitutionLoginController.php
@@ -2301,7 +2301,7 @@ public function assigncourserequest(Request $request)
  //dd($data7);
  $course_already_teacher_request_count=CourseTeacher::where(['user_id'=>$data_teacher['id'],'course_id'=>$course_lists['id']])->count();
 
-// dd($course_already_teacher_request_count);
+dd($course_already_teacher_request_count);
 
 
       $thearray = [];
