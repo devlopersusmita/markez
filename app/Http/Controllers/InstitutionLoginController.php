cf796129f6165a6c7a47f806116f60302e687def
devlopersusmita <susmita.elvirainfotech@gmail.com>
2023-09-27 11:22:42 +0530

    changes

--- app/Http/Controllers/InstitutionLoginController.php
+++ app/Http/Controllers/InstitutionLoginController.php
@@ -2355,7 +2355,7 @@ public function assigncoursetoteacher(Request $request)
     //$subscription_end_date = $user_details->subscription_end_date;
     // $institution_id = InstitutionAdmin::where('user_id',$user_id)->first()->institution_id;
     $course_lists = Course::where('institution_id',$user_id)->orderBy('title','asc')->get();
-    //dd($course_lists);
+    dd($course_lists);
    $data_teacher=InstitutionTeacher::leftjoin('users','institution_teachers.user_id','=','users.id')->where(['users.status'=>'active','users.role'=>'2','institution_teachers.institution_id'=>$user_id])->select('users.*')->orderBy('users.name','asc')->get();
    $t2_array =[];
    if(!empty($data_teacher) )
@@ -2376,6 +2376,7 @@ public function assigncoursetoteacher(Request $request)
  //dd($data7);
 
 
+ $data_exist_order=CourseTeacher::where(['user_id'=>$user_id,'course_id'=>$request->course_id])->count();
 
 
 
