a11079d9e2c281404807d0d497e75132d048cdd7
devlopersusmita <susmita.elvirainfotech@gmail.com>
2023-09-27 12:09:28 +0530

    institution login controller changes

--- app/Http/Controllers/InstitutionLoginController.php
+++ app/Http/Controllers/InstitutionLoginController.php
@@ -2299,7 +2299,7 @@ public function assigncourserequest(Request $request)
 
 
  //dd($data7);
- $course_already_teacher_request_count=CourseTeacher::where(['user_id'=>$data_teacher['id'],'course_id'=>$course_lists['id']])->count();
+
 
 //dd($course_already_teacher_request_count);
 
@@ -2430,7 +2430,8 @@ public function assigncoursetoteacherstore(Request $request)
                     }
                     else
                     {
-
+                        $course_already_teacher_request_count=CourseTeacher::where(['user_id'=>$data_teacher->id,'course_id'=>$course_lists->id])->count();
+                        dd($course_already_teacher_request_count);
 
                         $institution_id = $request->user_id;
 
