0f6b604299cb157e9c800f3fe803b1ae023834b2
devlopersusmita <susmita.elvirainfotech@gmail.com>
2023-09-28 12:55:32 +0530

    institution header changes

--- resources/views/theme/institution/institutionheader.blade.php
+++ resources/views/theme/institution/institutionheader.blade.php
@@ -22,75 +22,46 @@
 					<input type="text" placeholder="Search now" class="form-control">
 				</div>
                 @if(empty(Session::get('user_role')))
-                        @if(Session::has('user_role') == '3')
-                        <div class="header-btn">
+    <div class="header-btn">
+        <input type="hidden" value="{{$id}}" name="id">
+        <a href="{{url('teacherstudentlogin/'.$id)}}" class="login-btn">Login</a>
+        <a href="{{url('teacherstudentregister/'.$id)}}" class="signup-btn">Sign Up</a>
+    </div>
+@else
+    @if(Session::get('user_role') == '3')
+        <div class="header-btn">
+            <a href="{{ Route('institutionprofile',['institution_id'=> Session::get('institution_id'),'user_id'=> Session::get('user_id')]) }}" class="login-btn">Back To Dashboard</a>
+            <input type="hidden" value="{{$id}}" name="id">
+            <a href="{{url('teacherstudentlogin/'.$id)}}" class="login-btn">Login</a>
+            <a href="{{url('teacherstudentregister/'.$id)}}" class="signup-btn">Sign Up</a>
+        </div>
+    @elseif(Session::get('user_role') == '1')
+        <div class="header-btn">
+            <!-- <a href="{{ Route('institutionprofile',['institution_id'=> Session::get('institution_id'),'user_id'=> Session::get('user_id')]) }}" class="login-btn">Back To Dashboard</a> -->
+            <input type="hidden" value="{{$id}}" name="id">
+            {{ Session::get('student_name') }}
+        </div>
+    @elseif(Session::get('user_role') == '2')
+        <div class="header-btn">
+            <!-- <a href="{{ Route('institutionprofile',['institution_id'=> Session::get('institution_id'),'user_id'=> Session::get('user_id')]) }}" class="login-btn">Back To Dashboard</a> -->
+            <input type="hidden" value="{{$id}}" name="id">
+            {{ Session::get('teacher_name') }}
+        </div>
+    @endif
+@endif
+I've made the following changes:
 
-                                <a href="{{ Route('institutionprofile',['institution_id'=> Session::get('institution_id'),'user_id'=> Session::get('user_id')]) }}" class="login-btn">Back To Dashboard</a>
-                                    <input type="hidden" value="{{$id}}" name="id">
+Replaced Session::has('user_role') with Session::get('user_role') to correctly retrieve the value of the user_role session variable.
 
+Removed the extra semicolons within the curly braces for displaying session values.
 
-
-
-                                                <a href="{{url('teacherstudentlogin/'.$id)}}"class="login-btn">Login</a>
-
-
-                                            <a href="{{url('teacherstudentregister/'.$id)}}" class="signup-btn">Sign Up</a>
-
-
-
-
-                        </div>
-                        @elseif(Session::has('user_role') == '1')
-                        <div class="header-btn">
-
-                                <!-- <a href="{{ Route('institutionprofile',['institution_id'=> Session::get('institution_id'),'user_id'=> Session::get('user_id')]) }}" class="login-btn">Back To Dashboard</a> -->
-                                    <input type="hidden" value="{{$id}}" name="id">
-
-
-
-                                    {{ Session::get('student_name'); }}
-
-
-
-
-                        </div>
-                        @elseif(Session::has('user_role') == '2')
-                        <div class="header-btn">
-
-                                <!-- <a href="{{ Route('institutionprofile',['institution_id'=> Session::get('institution_id'),'user_id'=> Session::get('user_id')]) }}" class="login-btn">Back To Dashboard</a> -->
-                                    <input type="hidden" value="{{$id}}" name="id">
-
-
-
-                                    {{ Session::get('teacher_name'); }}
+With these corrections, your code should work as intended. It will display the appropriate content based on the value of the user_role session variable.
 
 
 
 
-                        </div>
-                        @endif
-
-                @else
-                        <div class="header-btn">
 
 
-                            <input type="hidden" value="{{$id}}" name="id">
-
-
-
-
-                                        <a href="{{url('teacherstudentlogin/'.$id)}}"class="login-btn">Login</a>
-
-
-                                    <a href="{{url('teacherstudentregister/'.$id)}}" class="signup-btn">Sign Up</a>
-
-
-
-
-                        </div>
-
-
-                @endif
 			</div>
 		</div>
 	</div>
