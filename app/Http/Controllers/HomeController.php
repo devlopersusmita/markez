c72445e88cb649bdf3af3f493565a1f4451d5f22
devlopersusmita <susmita.elvirainfotech@gmail.com>
2023-09-29 11:24:15 +0530

    home controller changes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2941,6 +2941,7 @@ public function postteacherstudentlogin(Request $request)
     //dd($user->id);
 
 
+    if ($user->role == 2 || $user->role == 1) {
         // Check for valid user credentials
         if ($user && Hash::check($password, $user->password)) {
             // Authentication successful
@@ -2986,8 +2987,10 @@ public function postteacherstudentlogin(Request $request)
             // Invalid credentials
             return redirect()->back()->withInput()->withErrors(['email' => 'Invalid email or password']);
         }
-
-
+    } else {
+        // Invalid credentials
+        return redirect()->back()->withInput()->withErrors(['email' => 'Invalid email or password']);
+    }
 
 
 
