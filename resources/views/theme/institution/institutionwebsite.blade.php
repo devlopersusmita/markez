988cdf8034b7fcf057376a14ade3bfa3dbee8588
devlopersusmita <susmita.elvirainfotech@gmail.com>
2023-09-29 20:02:18 +0530

    changes

--- resources/views/theme/institution/institutionwebsite.blade.php
+++ resources/views/theme/institution/institutionwebsite.blade.php
@@ -110,7 +110,7 @@
                                                             @endif
 
                                         <div class="couse-content">
-                                            <h3>{{ $course_data['title'] }}({{ $course_data['type'] }}</h3>
+                                            <h3>{{ $course_data['title'] }}({{ $course_data['type'] }})</h3>
 
                                             <p>
                         {!! Str::words($course_data['description'], 10, ' ...') !!}
