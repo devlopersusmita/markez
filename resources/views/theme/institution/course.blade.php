a955f862b9d510835203961ec0439519e5852630
devlopersusmita <susmita.elvirainfotech@gmail.com>
2023-09-29 13:26:16 +0530

    added

--- resources/views/theme/institution/course.blade.php
+++ resources/views/theme/institution/course.blade.php
@@ -92,8 +92,8 @@
 
                 <td class="course-action">
 
-                <input type="text" value="{{$user_id}}" name="user_id">
-                <input type="text" value="{{$user_ids}}" name="user_ids">
+                <input type="hidden" value="{{$user_id}}" name="user_id">
+                <input type="hidden" value="{{$user_ids}}" name="user_ids">
                                 <?php $course_id = $course["id"]; ?>
                                 <a href='{{Route("institutioncoursecontent",["id"=>$course_id,"institution_id"=>$user_id,"user_id"=>$user_ids])}}'  class="button is-solid blue-button raised"   style="cursor: pointer;"  >Contents</a>
 
