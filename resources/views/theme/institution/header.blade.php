2bc56e82f2bcfc3d09fb44ab96ca26b1c7d8fee4
devlopersusmita <susmita.elvirainfotech@gmail.com>
2023-10-03 12:53:05 +0530

    changes

--- resources/views/theme/institution/header.blade.php
+++ resources/views/theme/institution/header.blade.php
@@ -2,7 +2,7 @@
 <div class="app-container app-theme-white body-tabs-shadow fixed-sidebar fixed-header">
 	<div class="app-header header-shadow">
 		<div class="app-header__logo">
-			<div class="logo-src"><img src="images/dashbord.svg" alt="dashbord1"> DASHBOARD</div>
+			<div class="logo-src"><img src="images/dashbord.svg" alt="dashbord"> DASHBOARD</div>
 				<div class="header__pane ml-auto">
 					<div>
 						<button type="button" class="hamburger close-sidebar-btn hamburger--elastic"
