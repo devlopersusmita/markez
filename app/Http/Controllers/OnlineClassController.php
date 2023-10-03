16d0466016c48d6d921e565555f21fd5be2c77e2
devlopersusmita <susmita.elvirainfotech@gmail.com>
2023-10-03 12:14:58 +0530

    ONLINECLASS CHANGES

--- app/Http/Controllers/OnlineClassController.php
+++ app/Http/Controllers/OnlineClassController.php
@@ -269,7 +269,7 @@ public function attendancestore(Request $request,$id,$online_class_id)
 
          //$meeting = $this->createMeeting($request);
 
-             $user = Zoom::user()->first();
+         $user = Zoom::findById($request->user_id);
 
             /*  $user = Zoom::user()->create([
                     'first_name' => 'First Name',
