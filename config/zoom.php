924a39e7bea2bc12f2dfc311318e8812dabca112
devlopersusmita <susmita.elvirainfotech@gmail.com>
2023-10-03 11:33:48 +0530

    added

--- config/zoom.php
+++ config/zoom.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'api_key' => "V8wP_gitSaqfQe9vAYhMEA",
-    'api_secret' => 'MyZsDsc7hURuIGM0JpVIkBZ291bbE13LweML',
+    'api_key' => "LxUn4rXVSnOEfmu5bSw-Bg",
+    'api_secret' => 'uuphsIPk47knFnKLDx3ovwN1vUavfmZ6951r',
     'base_url' => 'https://api.zoom.us/v2/',
     'token_life' => 60 * 60 * 24 * 7, // In seconds, default 1 week
     'authentication_method' => 'jwt', // Only jwt compatible at present but will add OAuth2
