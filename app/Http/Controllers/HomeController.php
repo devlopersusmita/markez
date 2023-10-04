448c505a5919dd9ab561d48bead2ad7f3d8e59bd
devlopersusmita <susmita.elvirainfotech@gmail.com>
2023-10-04 16:25:48 +0530

    added

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -890,6 +890,7 @@ echo "<br>total_subscription=".$course->total_subscription;
 echo "<br>students_limitt=".$course->students_limitt;
 echo "<br>role=".Auth::user()->role;
 */
+dd(Session::get('user_role') == '1');
 if(($user_id > 0)  && ($course->total_subscription < $course->students_limit) && Session::get('user_role')->role == '1')
 {
 $data_exist_order=Order::where(['user_id'=>$user_id,'course_id'=>$id,'type'=>'course'])->count();
