743a8a9a30cf73101a542d53574649e2d0dc91ca
devlopersusmita <susmita.elvirainfotech@gmail.com>
2023-10-05 17:23:59 +0700

    saved

--- resources/views/theme/institution/subscription.blade.php
+++ resources/views/theme/institution/subscription.blade.php
@@ -47,7 +47,7 @@
 
 
                     if($current_date_time > $seven_days_before ){?>
-                           <a href="{{ route('subcription', ['institution_id' => $_GET['institution_id'],'user_id'=>$_GET['user_id']]) }}" class="button is-solid green-button raised" data-toggle="modal"   style="cursor: pointer;" >Renew</a>
+                           <a href="" class="button is-solid green-button raised" data-toggle="modal"   style="cursor: pointer;" >Renew</a>
                     <?php } ?>
 
                     </td>
