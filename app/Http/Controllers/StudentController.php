b3393c5392470d24e9c3d77de4851cfcb42c6024
devlopersusmita <susmita.elvirainfotech@gmail.com>
2023-10-05 11:48:01 +0530

    saved

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -518,7 +518,7 @@ class StudentController extends Controller
          //$institution_id =$request->institution_id;
 
         $users_subscription_exist = UserDetail::where(['user_id'=>$user_id,'user_type'=>'Student'])->whereDate('subscription_end_date', '>=', Carbon::now())->whereDate('subscription_start_date', '<=', Carbon::now())->count();
-        dd($users_subscription_exist);
+        //dd($users_subscription_exist);
         if($users_subscription_exist ==0)
         {
             return false;
@@ -984,8 +984,9 @@ class StudentController extends Controller
       public function coursecertificate(Request $request,$id,$download)
     {
         $course_id = $id;
+        $user_id = Session::get('user_id');
 
-        $check_student_subscription_exist = $this->check_student_subscription() ;
+        $check_student_subscription_exist = $this->check_student_subscription($user_id) ;
         $categories = Category::orderBy('name','asc')->get();
         $user_id = $request->user_id;
         //dd($user_id);
