a39541c74d3641f3e63b506b79f6b7a8b5434bff
devlopersusmita <susmita.elvirainfotech@gmail.com>
2023-10-05 17:15:17 +0700

    date

--- resources/views/theme/institution/subscription.blade.php
+++ resources/views/theme/institution/subscription.blade.php
@@ -39,11 +39,11 @@
             <a href="{{ URL::to('/#subscribe') }}" class="button is-solid green-button raised" data-toggle="modal"   style="cursor: pointer;" >View Plan</a>
 
             <?php
-                    $current_date_time = Carbon\Carbon::now();
+                    dd($current_date_time = Carbon\Carbon::now());
                     $current_date_time->toDateString();
                     $end_date =  strtotime($subscription['end_date']);
                     $end_date =  date('Y-m-d',$end_date);
-                   dd($seven_days_before = date('Y-m-d', strtotime('-7 day', strtotime($end_date))));
+                   $seven_days_before = date('Y-m-d', strtotime('-7 day', strtotime($end_date)));
 
 
                     if($current_date_time > $seven_days_before ){?>
