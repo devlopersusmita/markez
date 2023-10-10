2fff4f05134e647ff3c53c23e2b37060f4385cd0
devlopersusmita <susmita.elvirainfotech@gmail.com>
2023-10-10 17:55:55 +0700

    index page changes

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -128,7 +128,42 @@
 
 
             </div>
+            <div class="row">
 
+<div class="col-lg-3 col-6">
+
+    <!-- small box -->
+
+    <div class="small-box bg-info">
+
+        <div class="inner">
+
+            <h3>{{\App\Models\User::where('role',3)->count()}}</h3>
+
+            <p>Institutions</p>
+
+        </div>
+
+        <div class="icon">
+
+            <i class="ion ion-bag"></i>
+
+        </div>
+
+        <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+
+
+    </div>
+
+</div>
+
+
+
+
+
+
+
+</div>
             <!-- /.row -->
 
 
