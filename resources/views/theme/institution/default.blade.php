1b5dcf5b2b73f12d2d9893b7f1bda745ad71233c
devlopersusmita <susmita.elvirainfotech@gmail.com>
2023-10-11 12:20:25 +0700

    skill update

--- resources/views/theme/institution/default.blade.php
+++ resources/views/theme/institution/default.blade.php
@@ -6593,6 +6593,26 @@ $(document).on('click', '#enquiry_delete', function ()
 
 //end delete enquiry //
 
+// skill //
+$(document).ready(function() {
+
+$('input[name="input"]').tagsinput({
+    trimValue: true,
+    confirmKeys: [13, 44, 32],
+    focusClass: 'my-focus-class'
+});
+
+$('.bootstrap-tagsinput input').on('focus', function() {
+    $(this).closest('.bootstrap-tagsinput').addClass('has-focus');
+}).on('blur', function() {
+    $(this).closest('.bootstrap-tagsinput').removeClass('has-focus');
+});
+
+});
+
+
+//end skill //
+
 </script>
 
 
