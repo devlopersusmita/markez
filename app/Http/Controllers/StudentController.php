c5c901df58d3ab4b9ab0eabd678cdfd0934ac825
devlopersusmita <susmita.elvirainfotech@gmail.com>
2023-10-17 13:06:31 +0700

    student ocntroller changes

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -286,7 +286,7 @@ class StudentController extends Controller
 
         $course_content_quiz_details = Quiz::where('id',$course_content_quiz_id)->first();
         $all_q_id_array = explode(',', $course_content_quiz_details->all_questions);
-        //$user_id = Auth::id();
+        // $user_id = Auth::id();
         $user_id = Session::get('user_id');
         $data7=Question::
          orderBy('id','desc')
@@ -2211,13 +2211,7 @@ class StudentController extends Controller
 
         $institution_teacher_request->save();
 
-        $institution_teacher = new InstitutionTeacher();
-        $institution_teacher->institution_id = $institution_id;
-        $institution_teacher->user_id = $user_id;
-        $institution_teacher->status = 'Pending';
-        $institution_teacher->created_by = $institution_id;
 
-        $institution_teacher->save();
 
 
 
