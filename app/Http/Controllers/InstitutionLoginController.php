6a75f1ba31c0cfb520216d9f9ae48189c4999eb0
devlopersusmita <susmita.elvirainfotech@gmail.com>
2023-10-19 18:10:51 +0700

    added

--- app/Http/Controllers/InstitutionLoginController.php
+++ app/Http/Controllers/InstitutionLoginController.php
@@ -1978,8 +1978,19 @@ else{
     {
 
 
-
-
+                //$user_id=Auth::id();
+                if($request->institution_id == null) {
+                    $user_id = $_GET['institution_id'];
+                } else {
+                    $user_id = $request->institution_id;
+                }
+                //userstable id//
+                if($request->user_id == null) {
+                    $user_ids = $_GET['user_id'];
+                } else {
+                    $user_ids = $request->user_id;
+                }
+            dd($user_id,$user_ids);
 
              if(Auth::user())
             {
